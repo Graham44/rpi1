28accae4dfb69d9bb30fefbe14bf98d3c241a21e
Graham <Graham44@users.noreply.github.com>
2015-10-03 14:40:09 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Base software installed.  WiFi connected.
+sudo apt-get install emacs23-nox -y
+sudo apt-get install fbi -y
+sudo apt-get install sendemail -y
+sudo apt-get install libnet-ssleay-perl -y
+# Python install
+sudo pip install feedparser
+# Need PiGlow modules to be loaded
